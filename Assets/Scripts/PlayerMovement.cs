de654ba43d51552b54b8177b229fcde3e9c81b42
Michal Krolikowski <michal.krolikowski1@gmail.com>
2020-02-16 10:25:32 +0100

    Player ship and its movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float velocity;
+    private Camera _mainCamera;
+
+    protected void Awake()
+    {
+        _mainCamera = Camera.main;
+        Cursor.visible = false;
+    }
+
+    protected void Update()
+    {
+        var mousePosition = Input.mousePosition;
+        mousePosition = _mainCamera.ScreenToWorldPoint(mousePosition);
+        
+        var playerPosition = transform.position;
+        transform.position = new Vector3(
+            Mathf.Lerp(playerPosition.x, mousePosition.x, velocity * Time.deltaTime),
+            playerPosition.y,
+            playerPosition.z);
+    }
+}
