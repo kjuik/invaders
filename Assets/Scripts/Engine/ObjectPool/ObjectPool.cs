3e9f000b143614cd2498f8834795c53929f9736d
Michal Krolikowski <michal.krolikowski1@gmail.com>
2020-02-22 23:16:30 +0100

    Refreshed ObjectPool code

--- Assets/Scripts/Engine/ObjectPool/ObjectPool.cs
+++ Assets/Scripts/Engine/ObjectPool/ObjectPool.cs
@@ -1,5 +1,4 @@
 ﻿using UnityEngine;
-using System;
 using System.Collections.Generic;
 
 public class ObjectPool : MonoBehaviour
@@ -7,21 +6,24 @@ public class ObjectPool : MonoBehaviour
     private readonly Dictionary<int, Queue<PooledObject>> _poolsByPrefabId = new Dictionary<int, Queue<PooledObject>>();
     [SerializeField] private int poolChunkSize = 5;
 
+    public GameObject Rent(GameObject prefab, float maxLifetime) 
+        => Rent(prefab.transform, maxLifetime).gameObject;
+    
     public T Rent<T>(T prefab, float maxLifetime) where T : Component
     {
         var id = prefab.GetInstanceID();
-        
-        if (!_poolsByPrefabId.ContainsKey(id))
-            _poolsByPrefabId.Add(prefab.GetInstanceID(), new Queue<PooledObject>());
-        if (_poolsByPrefabId[id].Count == 0)
-            PopulatePool(prefab, id);
+        EnsurePoolContains(prefab, id);
 
         var ret = _poolsByPrefabId[id].Dequeue();
         ret.OnRent(maxLifetime);
-
         return ret.GetComponent<T>();
     }
-    public void Return(Component obj) => Return(obj.GetComponent<PooledObject>());
+    
+    public void Return(GameObject obj) 
+        => Return(obj.GetComponent<PooledObject>());
+    
+    public void Return(Component obj) 
+        => Return(obj.GetComponent<PooledObject>());
 
     private void Return(PooledObject obj)
     {
@@ -29,11 +31,20 @@ public class ObjectPool : MonoBehaviour
         obj.OnReturn();
     }
 
+    private void EnsurePoolContains<T>(T prefab, int id) where T : Component
+    {
+        if (!_poolsByPrefabId.ContainsKey(id))
+            _poolsByPrefabId.Add(id, new Queue<PooledObject>());
+        
+        if (_poolsByPrefabId[id].Count == 0)
+            PopulatePool(prefab, id);
+    }
+    
     private void PopulatePool<T>(T prefab, int id) where T : Component
     {
         for (var i = 0; i < poolChunkSize; ++i)
         {
-            var instance = Instantiate(prefab, transform, true);
+            var instance = Instantiate(prefab);
             instance.name = prefab.name + "(Pooled)";
 
             var pooledObj = instance.gameObject.AddComponent<PooledObject>();
